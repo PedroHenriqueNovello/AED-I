8fd37f030937559d796baad456d2fe1809213cb3
Pedro Henrique Novello D'Elia Porto de Almeida <pedro.porto04@unifesp.br>
2024-10-15 13:54:06 -0300

    Create 1020.c

---
+++ 1020.c
@@ -0,0 +1,17 @@
+// Pedro Henrique Novello D'Elia Porto de Almeida
+
+#include <stdio.h>
+
+int main() {
+    int idade, anos, meses, dias;
+    
+    printf("");
+    scanf("%d",&idade);
+    
+    anos = idade/365;
+    meses = (idade%365)/30;
+    dias = (idade%365)%30;
+    
+    printf("%d ano(s)\n%d mes(es)\n%d dia(s)\n", anos, meses, dias);
+    return 0;
+}

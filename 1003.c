88564e5a8c4bf8f2c2c9756440ef95faff814973
Pedro Henrique Novello D'Elia Porto de Almeida <pedro.porto04@unifesp.br>
2024-11-15 10:19:44 -0300

    Create 1003.c

---
+++ 1003.c
@@ -0,0 +1,12 @@
+//Pedro Henrique Novello D'Elia Porto de Almeida RA: 176620
+
+#include <stdio.h>
+ 
+int main() {
+    int A, B, soma;
+    scanf("%i %i", &A, &B);
+    soma = A + B;
+    printf("SOMA = %i\n", soma);
+ 
+    return 0;
+}

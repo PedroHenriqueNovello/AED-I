a8de82893457c4327d107fa00be483033328b6eb
Pedro Henrique Novello D'Elia Porto de Almeida <pedro.porto04@unifesp.br>
2024-11-21 16:37:38 -0300

    Create 1014.c

---
+++ 1014.c
@@ -0,0 +1,11 @@
+//Pedro Henrique Novello D'Elia Porto de Almeida RA: 176620
+#include <stdio.h>
+int main(){
+    float X, Y;
+    scanf("%f %f", &X, &Y);
+    
+    printf("%.3f km/l\n", X/Y);
+    return 0;
+
+
+}

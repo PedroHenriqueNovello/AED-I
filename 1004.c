70febf31a9689cc0b9ec99b1f90498657bcace4f
Pedro Henrique Novello D'Elia Porto de Almeida <pedro.porto04@unifesp.br>
2024-11-21 15:49:57 -0300

    Create 1004.c

---
+++ 1004.c
@@ -0,0 +1,11 @@
+//Pedro Henrique Novello D'Elia Porto de Almeida RA: 176620
+#include <stdio.h>
+ 
+int main() {
+    int A, B, prod;
+    scanf("%i %i", &A, &B);
+    prod = A * B;
+    printf("PROD = %i\n", prod);
+ 
+    return 0;
+}

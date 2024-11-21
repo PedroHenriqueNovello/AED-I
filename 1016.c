96e291d7d0c5c983eeabb69b1fe444bf71be0866
Pedro Henrique Novello D'Elia Porto de Almeida <pedro.porto04@unifesp.br>
2024-11-21 16:39:14 -0300

    Create 1016.c

---
+++ 1016.c
@@ -0,0 +1,9 @@
+//Pedro Henrique Novello D'Elia Porto de Almeida RA: 176620
+#include <stdio.h>
+int main(){
+    int dist, tempo;
+    scanf("%d", &dist);
+    tempo = dist * 2;
+    printf("%d minutos\n", tempo);
+    return 0;
+}

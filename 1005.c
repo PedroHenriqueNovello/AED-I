665a4e19374bb2630688d58382aa9fff0466f4dc
Pedro Henrique Novello D'Elia Porto de Almeida <pedro.porto04@unifesp.br>
2024-11-21 15:51:05 -0300

    Create 1005.c

---
+++ 1005.c
@@ -0,0 +1,14 @@
+//Pedro Henrique Novello D'Elia Porto de Almeida RA: 176620
+#include <stdio.h>
+ 
+int main() {
+    double A, B, media;
+    scanf("%lf %lf", &A, &B);
+    media = (3.5 * A + 7.5 * B) / 11;
+    printf("MEDIA = %.5f\n", media);
+    
+ 
+    
+ 
+    return 0;
+}

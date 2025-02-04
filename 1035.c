085c7a964ebd62e0aaa8506a5379e67eff2788ef
Pedro Henrique Novello D'Elia Porto de Almeida <pedro.porto04@unifesp.br>
2025-02-04 13:37:31 -0300

    Create 1035.c

---
+++ 1035.c
@@ -0,0 +1,16 @@
+// Pedro Henrique Novello D'Elia Porto de Almeida RA: 176620
+
+#include <stdio.h>
+
+int main(){
+	int A, B, C, D;
+	
+	scanf("%d %d %d %d", &A, &B, &C, &D);
+	
+	if((B>C) && (D>A) && (C+D > A+B) && (C>0) && (D>0) && (A%2 == 0)){
+		printf("Valores aceitos\n");
+	}
+	else{
+		printf("Valores nao aceitos\n");
+	}
+}

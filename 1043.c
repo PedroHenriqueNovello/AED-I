e8b56d49d11bbd95894c5df3f6f2c441ba71175c
Pedro Henrique Novello D'Elia Porto de Almeida <pedro.porto04@unifesp.br>
2025-02-04 13:56:23 -0300

    Create 1043.c

---
+++ 1043.c
@@ -0,0 +1,17 @@
+// Pedro Henrique Novello D'Elia Porto de Almeida RA: 176620
+#include <stdio.h>
+
+int main(){
+	float A, B, C, result;
+	
+	scanf("%f %f %f", &A, &B, &C);
+	
+	if(A<B+C && B<A+C && C<A+B){
+		result = A + B + C;
+		printf("Perimetro = %.1f\n", result);
+	}
+	else{
+		result = ((A+B)*C) / 2;
+		printf("Area = %.1f\n", result);
+	}
+}
